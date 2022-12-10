e9e391202de01c76646618c4b72847fa32b71ac7
Ildar Sampilov <ildar.sampilov@yandex.ru>
2022-12-10 17:04:58 +0600

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Цырендылыкова Эржена on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
